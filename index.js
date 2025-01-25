511c740e78845a9fdcea4cdab8cfe718992d099a
Glitch (workable-ajar-caravel) <none>
2025-01-25 09:07:22 +0000

    🌆📰 Checkpoint ./sistem.js:66007998/598 ./index.js:66007998/3085

--- index.js
+++ index.js
@@ -3,19 +3,33 @@
 const { spawn } = require('child_process');
 
 function hady() {
+
   const child = spawn("node Ayanokoji.js", {
+
     cwd: __dirname,
+
     stdio: "inherit",
+
     shell: true
+
   });
 
   child.on("close", (code) => {
+
     if (code == 2) {
+
       hady(); 
+
     }
+
   });
+
 }
+
 hady();
+
 setInterval(function() {
+
   console.clear();
+
 }, 3600000); 
