0aa01f7052c692390889d7f24ba4b8234b1c7fbf
Glitch (bouncy-wistful-grill) <none>
2025-02-04 11:42:09 +0000

    🚧🏂 Checkpoint

--- perintah/fish.js
+++ perintah/fish.js
@@ -4,8 +4,8 @@ const axios = require('axios');
 module.exports = {
   hady: {
     nama: "fish",
-    penulis: "Horikita",
-    kuldown: 10,
+    penulis: "Nahl",
+    kuldown: 20,
     peran: 0,
     tutor: "<cmd/kosong>"
   },
@@ -16,22 +16,28 @@ module.exports = {
       { nama: "Ikan Arwana", harga: 500, kesulitan: 3 },
       { nama: "Ikan Guppy", harga: 50, kesulitan: 1 },
       { nama: "Ikan Neon", harga: 100, kesulitan: 2 },
-      { nama: "Ikan Piranha", harga: 2000, kesulitan: 3 },
+      { nama: "Ikan Piranha", harga: 200, kesulitan: 3 },
       { nama: "Ikan Shark", harga: 500, kesulitan: 4 },
       { nama: "Ikan Tuna", harga: 100, kesulitan: 2 },
       { nama: "Ikan Salmon", harga: 200, kesulitan: 3 }
     ];
 
     if (args[0] === "beli") {
-      const hargaUmpan = 500;
-      const uangPengguna = await fs.readFile(`uang_${event.senderID}.txt`, 'utf8');
+      const hargaUmpan = 10;
+      let uangPengguna = 0;
+      try {
+        uangPengguna = await fs.readFile(`uang_${event.senderID}.txt`, 'utf8');
+      } catch (err) {
+        await fs.writeFile(`uang_${event.senderID}.txt`, '0');
+        uangPengguna = 0;
+      }
       if (uangPengguna >= hargaUmpan) {
         await fs.writeFile(`uang_${event.senderID}.txt`, uangPengguna - hargaUmpan);
         api.sendMessage(`Kamu berhasil membeli umpan!`, event.threadID, event.messageID);
       } else {
         api.sendMessage(`Kamu tidak memiliki uang yang cukup untuk membeli umpan!`, event.threadID, event.messageID);
       }
-    } else if (args[0] === "fish") {
+    } else if (args[0] === "mulai") {
       const kesulitanMancing = Math.floor(Math.random() * 5) + 1;
       const ikanDipilih = fish.find(ikan => ikan.kesulitan === kesulitanMancing);
       if (ikanDipilih) {
@@ -47,7 +53,7 @@ module.exports = {
         api.sendMessage(`Kamu gagal menangkap ikan!`, event.threadID, event.messageID);
       }
     } else {
-      api.sendMessage(`Kamu harus menggunakan perintah "beli" untuk membeli umpan atau "fish" untuk memulai mancing!`, event.threadID, event.messageID);
+      api.sendMessage(`Kamu harus menggunakan perintah "beli" untuk membeli umpan atau "mulai" untuk memulai mancing!`, event.threadID, event.messageID);
     }
   }
 };
