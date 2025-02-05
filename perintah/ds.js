9a9c0f3bc3a2619665ccef23199fcea29651d346
Glitch (bouncy-wistful-grill) <none>
2025-02-05 15:19:57 +0000

    😀🍩 Checkpoint ./perintah/ds.js:66007998/11737

--- perintah/ds.js
+++ perintah/ds.js
@@ -13,8 +13,11 @@ module.exports = {
   Ayanokoji: async function({ api, event }) {
     const args = event.body.split(' ');
     const command = args[0].toLowerCase();
-const dsInfo = async ({ api, event }) => {
-  const info = `
+
+    if (command === 'ds') {
+      if (args[1] === 'info') {
+        const dsInfo = async ({ api, event }) => {
+          const info = `
 Informasi Gacha Kartu Demon Slayer
 
 Kartu Demon Slayer
@@ -29,77 +32,34 @@ Kartu Demon Slayer
  - SR: 14¥
  - SSR: 28¥
 `;
-  api.sendMessage(info, event.threadID);
-}
-const dsPull = async ({ api, event }) => {
-  const userIndex = userData.findIndex(user => user.id === event.senderID);
-  if (userIndex === -1) {
-    userData.push({ id: event.senderID, yen: 0, cards: [] });
-  }
-  if (userData[userIndex].yen < 10) {
-    api.sendMessage('Saldo Anda tidak cukup untuk melakukan pull.', event.threadID);
-    return;
-  }
-  const dsInv = async ({ api, event }) => {
-  const userIndex = userData.findIndex(user => user.id === event.senderID);
-  if (userIndex === -1) {
-    api.sendMessage('Anda belum memiliki kartu.', event.threadID);
-  } else {
-    const cards = userData[userIndex].cards;
-    let message = 'Inventory Kartu Anda:\n';
-    cards.forEach((card, index) => {
-      message += `${index + 1}. ${card.nama}\n`;
-    });
-    api.sendMessage(message, event.threadID);
-  }
-};
-  
-const dsSell = async ({ api, event }) => {
-  const args = event.body.split(' ');
-  if (args[2] === 'c') {
-    const jumlah = parseInt(args[3]);
-    const cardName = args[4];
-    const userIndex = userData.findIndex(user => user.id === event.senderID);
-    if (userIndex === -1) {
-      api.sendMessage('Anda belum memiliki kartu.', event.threadID);
-    } else {
-      const cards = userData[userIndex].cards;
-      const cardIndex = cards.findIndex(card => card.nama === cardName);
-      if (cardIndex === -1) {
-        api.sendMessage(`Anda tidak memiliki kartu ${cardName}.`, event.threadID);
-      } else if (cards[cardIndex].jumlah < jumlah) {
-        api.sendMessage(`Anda tidak memiliki cukup kartu ${cardName} untuk dijual.`, event.threadID);
-      } else {
-        cards[cardIndex].jumlah -= jumlah;
-        api.sendMessage(`Anda berhasil menjual ${jumlah} kartu ${cardName}.`, event.threadID);
-      }
-    }
-  } else {
-    const cardName = args[2];
-    const userIndex = userData.findIndex(user => user.id === event.senderID);
-    if (userIndex === -1) {
-      api.sendMessage('Anda belum memiliki kartu.', event.threadID);
-    } else {
-      const cards = userData[userIndex].cards;
-      const cardIndex = cards.findIndex(card => card.nama === cardName);
-      if (cardIndex === -1) {
-        api.sendMessage(`Anda tidak memiliki kartu ${cardName}.`, event.threadID);
-      } else {
-        cards.splice(cardIndex, 1);
-        api.sendMessage(`Anda berhasil menjual kartu ${cardName}.`, event.threadID);
+          api.sendMessage(info, event.threadID);
+        };
+        dsInfo({ api, event });
+      } else if (args[1] === 'pull') {
+        const dsPull = async ({ api, event }) => {
+          const userIndex = userData.findIndex(user => user.id === event.senderID);
+          if (userIndex === -1) {
+            userData.push({ id: event.senderID, yen: 0, cards: [] });
+          }
+          if (userData[userIndex].yen < 10) {
+            api.sendMessage('Saldo Anda tidak cukup untuk melakukan pull.', event.threadID);
+            return;
+          }
+
+          userData[userIndex].yen -= 10;
+
+          const kartu = gacha();
+          userData[userIndex].cards.push(kartu);
+
+          fs.writeFileSync('./status.json', JSON.stringify(userData, null, 2));
+
+          api.sendMessage({
+            body: `Selamat, Anda mendapatkan kartu ${kartu.nama}! Saldo Anda sekarang: ${userData[userIndex].yen} yen.`,
+            attachment: fs.createReadStream(__dirname + `/cards/${kartu.nama}.jpg`)
+          }, event.threadID);
+        };
+        dsPull({ api, event });
       }
     }
   }
-};
-if (command === 'ds') {
-  if (args[1] === 'info') {
-    dsInfo({ api, event });
-  } else if (args[1] === 'pull') {
-    dsPull({ api, event });
-  } else if (args[1] === 'inv') {
-    dsInv({ api, event });
-  } else if (args[1] === 'sell') {
-    dsSell({ api, event });
-  }
-}
 };
