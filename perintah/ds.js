c467f562a4ecf1bd9cc4d13084aade0cc0d755b9
Glitch (bouncy-wistful-grill) <none>
2025-02-06 06:13:56 +0000

    🐣💰 Checkpoint ./perintah/ds.js:66007998/6775

--- perintah/ds.js
+++ perintah/ds.js
@@ -1,15 +1,10 @@
 const fs = require('fs');
 const path = require('path');
 const statusPath = './status.json';
-const dsData = require(path.join(__dirname, 'ds.json'));
+const dsData = './perintah/ds.json';
 
 const gacha = () => {
-  const rarityRates = {
-    R: 0.987,
-    SR: 0.302,
-    SSR: 0.032
-  };
-
+  const rarityRates = { R: 0.987, SR: 0.302, SSR: 0.032 };
   const getRarity = () => {
     const random = Math.random();
     if (random < rarityRates.SSR) {
@@ -20,12 +15,8 @@ const gacha = () => {
       return 'R';
     }
   };
-
   const rarity = getRarity();
-  const kartu = {
-    nama: 'Kartu Demon Slayer',
-    rarity: rarity
-  };
+  const kartu = { nama: dsData[Math.floor(Math.random() * dsData.length)].nama, rarity: rarity };
   return kartu;
 };
 
@@ -40,24 +31,11 @@ module.exports = {
   Ayanokoji: async function({ api, event }) {
     const args = event.body.split(' ');
     const command = args[0].toLowerCase();
+    const userData = require(statusPath);
 
     if (command === 'ds') {
       if (args[1] === 'info') {
-        const info = `
-Informasi Gacha Kartu Demon Slayer
-
-Kartu Demon Slayer
-
-• Drop Rate
- - R: 98.7%
- - SR: 30.2%
- - SSR: 3.2%
-
-• Harga Market
- - R: 8.5¥
- - SR: 14¥
- - SSR: 28¥
-`;
+        const info = ` Informasi Gacha Kartu Demon Slayer Kartu Demon Slayer • Drop Rate - R: 98.7% - SR: 30.2% - SSR: 3.2% • Harga Market - R: 8.5¥ - SR: 14¥ - SSR: 28¥ `;
         api.sendMessage(info, event.threadID);
       } else if (args[1] === 'pull') {
         const userIndex = userData.findIndex(user => user.id === event.senderID);
@@ -68,17 +46,21 @@ Kartu Demon Slayer
           api.sendMessage('Saldo Anda tidak cukup untuk melakukan pull.', event.threadID);
           return;
         }
-
         userData[userIndex].yen -= 10;
-
         const kartu = gacha();
+        const cardData = dsData.find(card => card.nama === kartu.nama);
         userData[userIndex].cards.push(kartu);
-
-        fs.writeFileSync('./status.json', JSON.stringify(userData, null, 2));
-
-        api.sendMessage({
-          body: `Selamat, Anda mendapatkan kartu ${kartu.nama}! Saldo Anda sekarang: ${userData[userIndex].yen} yen.`,
-        }, event.threadID);
+        fs.writeFileSync(statusPath, JSON.stringify(userData, null, 2));
+        if (cardData) {
+          api.sendMessage({
+            body: `Selamat, Anda mendapatkan kartu ${kartu.nama}! Saldo Anda sekarang: ${userData[userIndex].yen} yen.`,
+            attachment: cardData.gambar
+          }, event.threadID);
+        } else {
+          api.sendMessage({
+            body: `Selamat, Anda mendapatkan kartu ${kartu.nama}! Saldo Anda sekarang: ${userData[userIndex].yen} yen.`
+          }, event.threadID);
+        }
       } else if (args[1] === 'inv') {
         const userIndex = userData.findIndex(user => user.id === event.senderID);
         if (userIndex === -1) {
