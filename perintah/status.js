57258a8e0805968621e9b76f1c8c36579eb9d66e
Glitch (bouncy-wistful-grill) <none>
2025-02-16 06:03:50 +0000

    👦👄 Checkpoint ./perintah/status.js:67989344/103

--- perintah/status.js
+++ perintah/status.js
@@ -13,17 +13,18 @@ module.exports = {
         return api.sendMessage("❌ Gagal mengambil data pengguna. Silakan coba lagi nanti.", event.threadID);
       }
       // Hitung progress exp
+      const expProgress = (userData.exp / 2500 * 100).toFixed(2);
       const progressBar = "⬜".repeat(Math.floor(expProgress/10)) + "⬛".repeat(10 - Math.floor(expProgress/10));
       // Format pesan
       const message = `STATUS
        Name : ${userData.nama.toUpperCase()} 
        Yen: ${userData.yen.toFixed(2)} ¥ 
-       Exp: ${userData.exp}
+       Exp: ${userData.exp}/${2500}
        Level: ${userData.level} 
        Progress: ${progressBar} (${expProgress}%) 
-      api.sendMessage(message, event.threadID);
+     api.sendMessage(message, event.threadID);
     } catch (error) {
-      console.error("Error di command status:", error);
+      console.error("Error di command status:", err.message);
       api.sendMessage("❌ Gagal mengambil data status", event.threadID);
     }
   }
