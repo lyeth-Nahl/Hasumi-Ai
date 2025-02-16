12ebc928735e495c4cbd300d85413109170232fd
Glitch (bouncy-wistful-grill) <none>
2025-02-16 07:14:50 +0000

    🎅🏔 Checkpoint

--- perintah/setn.js
+++ perintah/setn.js
@@ -8,10 +8,14 @@ module.exports = {
   },
   bahasa: {
     id: {
-      hadi: "Kamu belum memberikan id nya."
+      hadi: "Kamu belum memberikan id nya.",
+      berhasil: "Nama kamu telah diubah menjadi",
+      yenKurang: "Yen kamu tidak cukup!"
     },
     en: {
-      hadi: "You haven't given her id yet."
+      hadi: "You haven't given her id yet.",
+      berhasil: "Your name has been changed to",
+      yenKurang: "You don't have enough yen!"
     }
   },
   Ayanokoji: async function ({ api, event, args, bhs }) {
@@ -19,16 +23,19 @@ module.exports = {
       api.sendMessage(bhs.hadi, event.threadID);
       return;
     }
-    const id = args[0];
+    const namaBaru = args.join(" ");
     const userData = await getData(event.senderID);
-    if (userData.yen < 0.5) {
-      api.sendMessage("Yen kamu tidak cukup!", event.threadID);
-      return;
+    if (userData) {
+      if (userData.yen < 0.5) {
+        api.sendMessage(bhs.yenKurang, event.threadID);
+        return;
+      }
+      userData.nama = namaBaru;
+      userData.yen -= 0.5;
+      await updateDatabase(`users/${event.senderID}`, userData);
+      api.sendMessage(`${bhs.berhasil} ${namaBaru}`, event.threadID);
+    } else {
+      api.sendMessage("Data pengguna tidak ditemukan", event.threadID);
     }
-    // Kurangi yen pengguna
-    setUser(event.senderID, "yen", userData.yen - 0.5);
-    // Simpan nama baru ke database
-    setUser(event.senderID, "nama", id);
-    api.sendMessage(`Nama kamu telah diubah menjadi ${id}. Yen kamu telah dikurangi sebesar 0.5`, event.threadID);
   }
 };
