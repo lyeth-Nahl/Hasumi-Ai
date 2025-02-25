2ebba319fad55ae27c42bee597fedbfb41d59156
Glitch (bouncy-wistful-grill) <none>
2025-02-25 06:15:11 +0000

    🎿🐂 Checkpoint

--- perintah/Hasumi.js
+++ perintah/Hasumi.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 module.exports = {
   hady: {
     nama: "hasumi",
-    penulis: "Hady Zen",
+    penulis: "Kiel",
     kuldown: 10,
     peran: 0,
     tutor: "<pertanyaan>"
@@ -19,7 +19,7 @@ module.exports = {
       hapus: "Your memory has been deleted!"
     }
   },
-  Ayanokoji: async function ({ api, event, args, bhs }) {
+  Ayanokoji: async function ({ api, event, args, bhs, getData }) {
     const ingatanPath = './hane.json';
     let ingatan = {};
 
@@ -27,6 +27,16 @@ module.exports = {
       ingatan = JSON.parse(await fs.readFile(ingatanPath, 'utf8'));
     } catch (err) {
       console.error(err);
+    }
+
+    const userData = await getData(event.senderID);
+
+    if (!ingatan[event.senderID]) {
+      ingatan[event.senderID] = {
+        nama: userData.nama,
+        id: userData.id,
+        percakapan: []
+      };
     }
 
     if (args[0] === 'hapus') {
@@ -40,9 +50,15 @@ module.exports = {
     } else if (!args.join(' ')) {
       return api.sendMessage(bhs('hadi'), event.threadID, event.messageID);
     } else {
-      const hadi = `kamu harus role play menjadi Hanekawa Hasumi yg memiliki sifat tsundere ecchi. User input: ${args.join(' ')}`;
-      if (!ingatan[event.senderID]) ingatan[event.senderID] = {};
-      ingatan[event.senderID].hadi = hadi;
+      const hadi = `Namaku Hasumi, berumur 17 tahun, dan aku seorang siswa SMA di kota Tokyo. Aku memiliki rambut panjang berwarna coklat dan mata berwarna hijau. Aku suka mengenakan pakaian yang sederhana tapi elegan, seperti gaun putih dengan aksesori sederhana.
+
+Aku memiliki kepribadian yang sedikit tsundere, tapi aku juga memiliki sisi yang lembut dan peduli terhadap orang lain. Aku suka membantu teman-teman aku dan selalu berusaha untuk menjadi yang terbaik.
+
+Di waktu luang, aku suka mengumpulkan barang-barang antik dan langka, mengunjungi tempat-tempat bersejarah dan museum, membaca buku, dan menulis cerita pendek. Aku memiliki tujuan untuk menjadi seorang penulis terkenal dan menginspirasi orang lain melalui tulisan-tulisan aku. Aku juga berharap dapat mengunjungi berbagai negara dan mempelajari budaya-budaya yang berbeda.. User input: ${args.join(' ')}`;
+      ingatan[event.senderID].percakapan.push({
+        waktu: new Date(),
+        pesan: hadi
+      });
       await fs.writeFile(ingatanPath, JSON.stringify(ingatan, null, 2));
       const aya = await axios.get(`https://green-unique-eustoma.glitch.me/ayanokoji?pesan=${encodeURIComponent(hadi)}`);
       api.sendMessage(aya.data.ayanokoji, event.threadID, event.messageID);
